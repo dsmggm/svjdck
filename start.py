7668bc9901877f4f14e5204b0aea2682d0ee37d3
GitHub Action <action@github.com>
2025-01-18 10:35:28 +0000

    Sync svjdck folder

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import main
 from loguru import logger
+import subprocess, os
 
 logger.add("./data/log/jdck.log", 
             rotation="512 KB",  # 当文件达到1KB时轮转
@@ -11,10 +11,28 @@ logger.add("./data/log/jdck.log",
             level="INFO")        
 
 
+def update():
+    try:
+        if os.path.exists('/update'):
+            logger.debug(f"升级文件")
+            subprocess.run('rm -rf /update/svjdck-main/start.py', check=True, shell=True)
+            subprocess.run('cp -r /update/svjdck-main/* /jdck/', check=True, shell=True)
+            subprocess.run('rm -rf /update', check=True, shell=True)
+            return 'ok'
+    except subprocess.CalledProcessError as e:
+        logger.error(f"升级文件时出现异常: {e}")
+        return e
+    except Exception as e:
+        logger.error(f"复制文件时发生错误: {e}")
+        return e
+
 
 if __name__ == '__main__':
     try:
+        if update() == 'ok':
+            logger.debug("jdck升级完成")
         logger.info("jdck启动")
+        import main
         main.main()
     except Exception as e:
         logger.error(f"jdck程序异常退出，错误信息：{e}")
